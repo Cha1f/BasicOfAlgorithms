db7c71d50ecb585e9ead5572f7c48ff7ab0a0aa6
Рома <pomo4444ka@gmail.com>
2023-07-02 15:42:37 +0300

    initial commit

---
+++ src/main/java/com/basicofalgorithms/demobasicofalgorithms/DemobasicofalgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.basicofalgorithms.demobasicofalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobasicofalgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemobasicofalgorithmsApplication.class, args);
+    }
+
+}
